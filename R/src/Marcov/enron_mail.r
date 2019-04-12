e9c656fd49eb1160d3f9dc63ba2230e3547e43c6
aghiles <githubfortyuds@gmail.com>
2019-04-12 10:15:43 +0200

    commit

--- R/src/Marcov/enron_mail.r
+++ R/src/Marcov/enron_mail.r
@@ -11,8 +11,9 @@ install.packages('markovchain')
 
 
 devtools::install_github('spedygiorgio/markovchain')
-library(markovchain)
+6library(markovchain)
 library(dplyr)
 # SDR Funnel is our sales representative stages, AE Funnel is our account executive stages, and CW is a successfully closed deal
 seq <- c('SDR Funnel','SDR Funnel','AE Funnel','AE Funnel','AE Funnel','AE Funnel','AE Funnel','AE Funnel','CW')
 verifyMarkovProperty(seq)
+# 

a61e43e338a67a88b7e3dae2d284de03c720002b
aghiles <githubfortyuds@gmail.com>
2019-07-05 19:07:17 +0200

    commit

---
+++ boards/arduino2k/libraries/Arduino_LoRaWAN_by_Beelan/src/arduino-rfm/Config.h
@@ -0,0 +1,12 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+// LoRaWAN freq. band
+// #define AS_923
+// #define EU_868
+#define US_915
+    //Select the subband youre working on
+    //#define SUBND_1
+    #define SUBND_6
+
+#endif
